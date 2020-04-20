3e15002bd7cab353f3601ef84cc5f0ae540c962a
Jukka Salminen <jukka@buena.fi>
2020-04-20 14:14:02 +0300

    Starting point for tictactoe game

---
+++ src/TicTacToe.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import gameStyles from './TicTacToe.module.scss';
+
+function TicTacToe() {
+	return (
+		<div>
+			TicTacToe
+		</div>
+	)
+};
+
+export default TicTacToe;
