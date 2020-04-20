48fa276a90282978a41620e5fd033a20fbb314c6
Jukka Salminen <jukka@buena.fi>
2020-04-20 14:35:03 +0300

    Add a documentation to explain what we're going to do

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -10,4 +10,26 @@ function TicTacToe() {
 	)
 };
 
+/*
+
+Requirements:
+---
+
+* A game board component, which has
+	- a game state, which has
+		- a board state
+		- a turn state
+	- a handleClick function 
+		- makes a copy of the board state
+		- adds an X or O to the copy
+		- calculates what the next turn is
+		- sets the state of the board and turn
+	- a function that calculates the winner
+	
+* A square component
+	- has a value inside the square that takes in props
+	- has an onClick function that takes in props
+
+*/
+
 export default TicTacToe;
